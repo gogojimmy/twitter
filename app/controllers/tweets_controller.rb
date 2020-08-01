97c7b55aa6f215f888d4b60c07dcfd1140d33edd
gogojimmy <jimmy@gogojimmy.net>
2020-08-01 17:08:55 +0800

    devise

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,16 @@
 class TweetsController < ApplicationController
+	before_action :authenticate_user!, except: %i[index]
+
 	def index
 		@tweet = Tweet.new
 		@tweets = Tweet.all
+		@edit_tweet = Tweet.first
 	end
 
 	def create
 		@tweet = Tweet.new(permitted_params)
+		@tweet.user = current_user
+
 		if @tweet.save
 			redirect_to tweets_path
 		else
